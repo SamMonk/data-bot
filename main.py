bc0a6eaf7138aeb970ea2aa5a64d6f31799c1698
Sam Monk <monk_sam@hotmail.com>
2020-08-07 20:06:13 -0400

    get s&p data

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+from wiki_stocks import get_wiki_sp
+
 print("Starting script")
+
+get_wiki_sp.save_sp500_tickers()
--- wiki_stocks/get_wiki_sp.py
+++ wiki_stocks/get_wiki_sp.py
@@ -6,15 +6,19 @@ def save_sp500_tickers():
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
     table = soup.find('table', {'class': 'wikitable sortable'})
-    tickers = []
+    tickers = {"stocks":[]}
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text.strip()
-        tickers.append(ticker)
+        sector = row.findAll('td')[3].text.strip()
+        industry = row.findAll('td')[4].text.strip()
+        print(row.findAll('td')[2])
+        reports = row.findAll('td')[2].find('a')['href'].strip()
+        tickers['stocks'].append({"ticker":ticker,"sector":sector,"industry":industry,"reports":reports})
         print(ticker)
         
-    with open("wiki-stocks/sp500tickers.json","w") as f:
+    with open("wiki_stocks/sp500tickers.json","w") as f:
         json.dump(tickers,f)
         
     return tickers
 
-save_sp500_tickers()
+#save_sp500_tickers()
