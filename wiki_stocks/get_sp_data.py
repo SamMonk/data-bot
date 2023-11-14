6ad29f02b8e7136104a2a0c9cc5066a9bdb6fe3e
Sam Monk <monk_sam@hotmail.com>
2023-11-13 20:11:06 -0500

    data bot is having trouble with yahoo data

--- wiki_stocks/get_sp_data.py
+++ wiki_stocks/get_sp_data.py
@@ -10,33 +10,52 @@ import time
 
 engine = create_engine('sqlite:///wiki_stocks/wiki.db')
 Session = sessionmaker(bind=engine)
-
-
+# https://finance.yahoo.com/quote/FAF/analysis?p=FAF
+# https://ycharts.com/indicators/us_coporate_aaa_effective_yield
 Base.metadata.create_all(engine)
 session = Session()
 with open('./wiki_stocks/sp500tickers.json') as f:
     sp_data = json.load(f)
     for stock in sp_data['stocks']:
-        time.sleep(2)
+        time.sleep(5)
         print(stock["ticker"])
         yahoo_data = requests.get(
             f'https://finance.yahoo.com/quote/{stock["ticker"]}', headers={'User-Agent': 'Custom'})
-        print(yahoo_data)
+        # print(yahoo_data)
         soup = BeautifulSoup(yahoo_data.content, "html.parser")
         tables = soup.find_all("table")
         for table in tables:
             trs = table.find_all("tr")
             for tr in trs:
                 try:
-                    print(stock)
-                    print(tr)
+                    # print(stock)
+                    # print(tr)
                     session.merge(SPData(stock["ticker"], tr.find_all('td')[
                               0].text, datetime.datetime.now(), tr.find_all('td')[1].text))
-                    print(datetime.datetime.now())
-                    print("key:" + tr.find_all('td')[0].text)
+                    # print(datetime.datetime.now())
+                    # print("key:" + tr.find_all('td')[0].text)
+                    # print("value:" + tr.find_all('td')[1].text)
+                except Exception as ex:
+                    print(ex)
+                    print(stock["ticker"])
+            session.commit()
+        yahoo_analysis = requests.get(
+            f'https://finance.yahoo.com/quote/{stock["ticker"]}/analysis', headers={'User-Agent': 'Custom'})
+        # print(yahoo_analysis)
+        soup_analysis = BeautifulSoup(yahoo_analysis.content, "html.parser")
+        tables_analysis = soup_analysis.find_all("table")
+        for table in tables_analysis:
+            trs = table.find_all("tr")
+            for tr in trs:
+                try:
+                    # print(stock)
+                    # print(tr)
+                    session.merge(SPData(stock["ticker"], tr.find_all('td')[
+                              0].text, datetime.datetime.now(), tr.find_all('td')[1].text))
+                    # print(datetime.datetime.now())
+                    # print("key:" + tr.find_all('td')[0].text)
                     print("value:" + tr.find_all('td')[1].text)
                 except Exception as ex:
                     print(ex)
                     print(stock["ticker"])
             session.commit()
-            
